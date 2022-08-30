fdba8883e774e9aa0156fb1fd772b42f5bde0c80
Jabba Laci <jabba.laci@gmail.com>
2022-08-30 12:08:56 +0200

    location

---
+++ 06/location.py
@@ -0,0 +1,20 @@
+"""
+A junior Python programmer wants to have a user input a location
+and then have the location print on the screen.
+In addition, the developer wants to tell the user to enter
+North, South, East, or West for a location and have the user
+try again if a proper location is not entered or
+print the location if the location is North, South, East, or West.
+
+location = ["North", "South", "West", "East"]
+response = input("Enter North, South, West, or East for a location: ")
+"""
+
+def main():
+    # TODO
+    pass
+
+##############################################################################
+
+if __name__ == "__main__":
+    main()
