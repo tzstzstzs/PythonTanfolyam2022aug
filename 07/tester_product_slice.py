5318ff308edaa26500de3baa68e5750aba8fac54
Jabba Laci <jabba.laci@gmail.com>
2022-09-01 12:03:20 +0200

    7. nap, előkészület

---
+++ 07/tester_product_slice.py
@@ -0,0 +1,18 @@
+"""
+A tester wants to select every fifth item in a tuple
+for functional testing,
+starting with the second item in the tuple and
+ending at the ninth item in the tuple.
+
+Help: the 9th item is NOT included
+Help: expected output: ('2222', '7777')
+"""
+
+def main():
+    products = ('1111', '2222', '3333', '4444', '5555', '6666', '7777', '8888', '9999')
+    # TODO
+
+##############################################################################
+
+if __name__ == "__main__":
+    main()
